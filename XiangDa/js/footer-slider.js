bf8f1d4c2c5b104d1ca5eef53058351253739f13
RaiyenXiao <xiaolinna0625@163.com>
2017-03-16 10:36:50 +0800

    湘达物流官网开发

---
+++ XiangDa/js/footer-slider.js
@@ -0,0 +1,31 @@
+/* ======================================================================
+ * @description footer商家滚动 js文件
+ * @author Raiyen
+ * @time 20160318
+ * ======================================================================
+ */
+$(function(){
+	var scroll = 100;//移动距离
+    var scrollWidth = $(".scrol").width();
+	var leftMax = scrollWidth - 940;
+	$(".arrow-right").click(function(e){
+		var leftoffset = $(".scrol").position().left;
+		if(!$(".partners .scrol").is(":animated")){
+			if(leftoffset < -leftMax){
+				$(".partners .scrol").stop();
+			}else{
+				$(".partners .scrol").animate({left : "-=" + scroll +"px"},600);
+			}
+		}
+	});
+	$(".arrow-left").click(function(){
+		var leftoffset = $(".scrol").position().left;
+		if(!$(".partners .scrol").is(":animated")){
+			if(leftoffset == 0){
+				$(".partners .scrol").stop();
+			}else{
+				$(".partners .scrol").animate({left : "+=" + scroll +"px"},600);
+			}
+		}
+	});
+})
